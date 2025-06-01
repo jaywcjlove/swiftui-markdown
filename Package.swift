9ed92dd3ceca657ddeb1344cf419fa7034bb013b
David Hakki <david@etcapps.co>
2025-06-01 15:46:10 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         .iOS(.v14),
         .macOS(.v11),
         .visionOS(.v1),
-        .tvOS(.v12)
+        .tvOS(.v12),
+        .watchOS(.v4)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
