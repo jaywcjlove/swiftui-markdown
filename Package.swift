a4141969a74d510eb35d9f61d88a3774944fd318
DavidH <david.hakki@elitetechnicalcare.com>
2025-06-01 23:38:57 -0500

    Added VisionOS support (#12)
    * Update Package.swift
    
    * Added tvOS
    
    * Update Package.swift
    
    * Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,8 @@ let package = Package(
     name: "Markdown",
     platforms: [
         .iOS(.v14),
-        .macOS(.v11)
+        .macOS(.v11),
+        .visionOS(.v1),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
